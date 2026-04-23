a406ca93681770165aaca40cff93e72a8ba9d3fc
carl78-inf <carlos.salmeron@edu.uah.es>
2026-04-23 19:06:41 +0200

    tilemap de prueba subido

--- tests/tilemap_tests.py
+++ tests/tilemap_tests.py
@@ -1,1 +1,176 @@
 # Pruebas Tile Map
+import math
+
+from pathlib import Path
+
+import arcade
+
+# Constants
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Platformer"
+
+# Constants used to scale our sprites from their original size
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
+
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
+
+# Constants used to track the direction a character is facing
+RIGHT_FACING = 0
+LEFT_FACING = 1
+
+class Player(arcade.Sprite):
+
+    def __init__(self):
+        super().__init__(":resources:/images/alien/alienBlue_walk1.png")
+
+
+class GameView(arcade.View):
+    """
+    Main application class.
+    """
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.player = Player()
+
+        self.tile_map = None
+
+
+        self.scene = None
+
+        # A variable to store our camera object
+        self.camera = None
+
+        # A variable to store our gui camera object
+        self.gui_camera = None
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+        layer_options = {
+            "Platforms": {
+                "use_spatial_hash": True
+            }
+        }
+
+        # Load our TileMap
+        self.tile_map = arcade.load_tilemap(
+            "assets/map1.json",
+            scaling=TILE_SCALING,
+            layer_options=layer_options,
+        )
+
+        # Create our Scene Based on the TileMap
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
+        self.player.center_x = 128
+        self.player.center_y = 128
+        self.scene.add_sprite("Player", self.player)
+
+        # Create a Platformer Physics Engine, this will handle moving our
+        # player as well as collisions between the player sprite and
+        # whatever SpriteList we specify for the walls.
+        # It is important to supply static to the walls parameter. There is a
+        # platforms parameter that is intended for moving platforms.
+        # If a platform is supposed to move, and is added to the walls list,
+        # it will not be moved.
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite,
+            walls=self.scene["Walls"],
+            gravity_constant=GRAVITY,
+        )
+
+        # Initialize our camera, setting a viewport the size of our window.
+        self.camera = arcade.Camera2D()
+
+        # Initialize our gui camera, initial settings are the same as our world camera.
+        self.gui_camera = arcade.Camera2D()
+
+        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+
+        self.window.background_color = self.tile_map.background_color
+
+    def on_show_view(self):
+        self.setup()
+
+    def on_draw(self):
+        """Render the screen."""
+
+        # Clear the screen to the background color
+        self.clear()
+
+        # Activate our camera before drawing
+        self.camera.use()
+
+        # Draw our Scene
+        self.scene.draw()
+
+        # Activate our GUI camera
+        self.gui_camera.use()
+
+    def on_update(self, delta_time):
+        """Movement and Game Logic"""
+
+        # Move the player using our physics engine
+        self.physics_engine.update()
+
+        # Center our camera on the player
+        self.camera.position = self.player.position
+
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.ESCAPE:
+            self.setup()
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player.change_y = - PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called whenever a key is released."""
+
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player.change_x = 0
+        elif key == arcade.key.UP or key == arcade.key.W:
+            self.player.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player.change_y = 0
+
+def main():
+    """Main function"""
+    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+    menu_view = GameView()
+    window.show_view(menu_view)
+    arcade.run()
+
+
+if __name__ == "__main__":
+
+    # Obtenemos la ruta del proyecto utilizando PathLib,
+    # necesitamos esta ruta para poder acceder a los archivos con recursos
+    # de forma independiente desde donde se ejecute el script.
+    PROJECT_ROOT = Path(__file__).parent.parent
+
+    print(f"Project root is: {PROJECT_ROOT}")
+
+    # Ejemplo de acceso a un archivo dentro de recursos
+    filetest = PROJECT_ROOT / "assets" / "dialogs.txt"
+    print(f"Test file size: {filetest.stat().st_size} bytes")
+    
+
+    main()
