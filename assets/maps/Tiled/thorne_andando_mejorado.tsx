5bf1cc275289fa34362461bbfa902e8b204ff946
carl78-inf <carlos.salmeron@edu.uah.es>
2026-05-14 18:30:21 +0200

    ptoyecto Tiled integrado en carpeta maps

---
+++ assets/maps/Tiled/thorne_andando_mejorado.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="thorne_andando_mejorado" tilewidth="64" tileheight="64" tilecount="7" columns="7">
+ <image source="../../player/thorne_andando_mejorado.png" width="448" height="64"/>
+</tileset>
