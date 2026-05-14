f628accabd48be6cf97cb3d79f829a49fdcb05d4
Mario <mario.estebans@edu.uah.es>
2026-05-14 17:01:00 +0200

    objeto boton

---
+++ src/tile/boton.py
@@ -0,0 +1,41 @@
+from typing import Callable
+from pathlib import Path
+import sys
+import os
+
+# Obtiene la ruta de 'proyecto/src' (subiendo un nivel desde 'proyecto/src/tile')
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+# Ahora puedes importar util.io
+from util import io
+import arcade
+import config.controles as controles
+from objeto_evento import ObjetoEvento
+from entidad.jugador import Jugador
+
+class Boton(ObjetoEvento):
+    _SCALE: int = 1
+    def __init__(self, color: str, interaccion_pulsar: Callable[[], None], interaccion_soltar: Callable[[], None], center_x = 0, center_y = 0, angle = 0, **kwargs) -> None:
+        path_normal = Path("assets") / "images" / f"boton_{color}_normal.png"
+        path_pulsado = Path("assets") / "images" / f"boton_{color}_pulsado.png"
+
+        super().__init__(interaccion_pulsar, interaccion_soltar, path_normal, self._SCALE, center_x, center_y, angle, **kwargs)
+        self._pulsado: bool = False
+
+        textura_pulsado = arcade.texture.default_texture_cache.load_or_get_texture(path_pulsado)
+        self.append_texture(textura_pulsado)
+
+    def on_collide(self, entidad: arcade.Sprite) -> None:
+        if not isinstance(entidad, Jugador):
+            return
+
+        if io.tecla_justo_pulsada(controles.palanca_interactuar):
+            self._interaccion1() if self._activada else self._interaccion2()
+            self._toggle_activada()
+    
+    def _toggle_activada(self) -> None:
+        self._activada = not self._activada
+        self.set_texture(int(self._activada))
