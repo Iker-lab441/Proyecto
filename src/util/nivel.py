49814c45accc85ada36ae86126c3dc8067f8110e
carl78-inf <carlos.salmeron@edu.uah.es>
2026-05-16 12:05:32 +0200

    reorganización módulo nivel

--- src/util/nivel.py
+++ src/util/nivel.py
@@ -6,78 +6,6 @@ from entidad.jugador import Jugador
 from util.camara import Camara
 
 TILE_SCALING = 1
-
-"""BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-def _set_bloques(dict_bloques: dict, nivel:str) -> arcade.Scene:
-    bloques = dict_bloques
-    ruta = BASE_DIR / "assets" / "maps" / "bloques.json"
-    with open (ruta, "w", newline="") as archivo:
-        json.dump(bloques, archivo, indent=4, sort_keys=True)
-    ruta = BASE_DIR / "assets" / "maps" / "bloques.json"
-    tile_map = arcade.load_tilemap(
-        ruta,
-        scaling=TILE_SCALING,
-        layer_options= _set_layer_options(bloques),
-    ) 
-    #print(tile_map.width*tile_map.tile_width)
-    scene = arcade.Scene.from_tilemap(tile_map)
-    return scene
-
-def _set_layer_options(bloques:dict):
-        layer_options = {}
-        for layer in bloques["layers"]:
-            if(layer["name"] == "Muros"):
-                layer_options[layer["name"]] = {"use_spatial_hash": True}
-        #print(layer_options)
-        return layer_options
-
-def crear_nivel(nivel:str)-> arcade.Scene:
-    ruta = BASE_DIR / "assets" / "maps" / str(nivel+".json")
-    with open(ruta, 'r', encoding='utf-8') as archivo:
-        dict_nivel = json.load(archivo)
-        print(buscar_capa(dict_nivel, "Algo"))
-        for layer in dict_nivel["layers"]:
-            # Dibujar los bloques
-            if layer["name"] == "Bloques":
-                dict_bloques = dict_nivel
-                dict_bloques["layers"] = buscar_capa(dict_bloques, "Bloques")["layers"]
-                scene = _set_bloques(dict_bloques, nivel)
-            if layer["name"] == "Entidades":
-                for entidad in layer["layers"]:
-                    if entidad["name"] == "Jugador":
-                        marcador = entidad["objects"][0]
-                        #print(marcador["x"], marcador["y"])
-                        #jugador = Jugador(center_x=marcador["x"], center_y=marcador["y"])
-                        jugador = Jugador(center_x=300, center_y=300)
-                        #print(jugador)
-                        #print(marcador["name"])
-            #print(layer["name"])
-            #for layer in layer["layers"]:
-                #print("    - " + layer["name"])
-        #scene.add_sprite("Jugador", jugador)
-        #print(scene.get_sprite_list("Jugador")[0].center_x)
-    return [scene, jugador]
-
-def buscar_capa(dict_nivel: dict, nombre: str):
-    encontrado = False
-    i = 0
-    resultado = []
-    while not encontrado and i < len(dict_nivel["layers"]):
-        layer = dict_nivel["layers"][i]
-        if layer["name"] == nombre:
-            resultado = layer
-            encontrado = True
-        else:
-            if layer["type"] == "group":
-                resultado = buscar_capa(layer, nombre)
-                if resultado != []: encontrado = True
-        i += 1
-    return resultado"""
-
-    
-# Path = Path("assets") / "images" / "palanca1.png"
-
 class Tilemap():
     def __init__(self, path: Path):
         with open(path, 'r', encoding='utf-8') as archivo:
@@ -133,14 +61,16 @@ class Nivel(arcade.View):
     
     def crear_nivel(self) -> arcade.Scene:
 
-        def _layer_options(dict) -> dict:
-            layer_options = {}
-            for layer in tilemap._layers(dict):
-                if(layer == "Muros"):
-                    layer_options[layer] = {"use_spatial_hash": True}
-            return layer_options
-        
+        #-----------------------------------------------------------------------------------------------------------------#
         def _crear_escena(tilemap: Tilemap) -> arcade.Scene:
+
+            def _layer_options(dict) -> dict:
+                layer_options = {}
+                for layer in tilemap._layers(dict):
+                    if(layer == "Muros"):
+                        layer_options[layer] = {"use_spatial_hash": True}
+                return layer_options
+            
             bloques = tilemap.dict.copy()
             bloques["layers"] = tilemap._layer("Bloques")["layers"]
             ruta = Path("assets") / "maps" / "bloques.json"
@@ -157,15 +87,35 @@ class Nivel(arcade.View):
         def _append_jugador(tilemap: Tilemap, scene: arcade.Scene):
             altura = tilemap.dict["height"] * tilemap.dict["tileheight"]
             jugador_dict = tilemap._layer("Jugador")
+            for objeto in jugador_dict["objects"]:
+                if objeto["type"] == "Jugador":
+                    jugador = Jugador(scale=objeto["height"]/64, center_x=objeto["x"], center_y=altura - objeto["y"])
+            scene.add_sprite("Jugador", jugador)
+
+        def _append_objetos(tilemap: Tilemap, scene: arcade.Scene):
+
+            def _append_objetos_evento(tilemap: Tilemap, scene: arcade.Scene):
+                altura = tilemap.dict["height"] * tilemap.dict["tileheight"]
+                jugador_dict = tilemap._layer("Jugador")
+                for objeto in jugador_dict["objects"]:
+                    if objeto["type"] == "Jugador":
+                        jugador = Jugador(scale=objeto["height"]/64, center_x=objeto["x"], center_y=altura - objeto["y"])
+                scene.add_sprite("Jugador", jugador)
+
+            altura = tilemap.dict["height"] * tilemap.dict["tileheight"]
+            jugador_dict = tilemap._layer("Jugador")
             print(jugador_dict)
             for objeto in jugador_dict["objects"]:
                 if objeto["type"] == "Jugador":
                     jugador = Jugador(scale=objeto["height"]/64, center_x=objeto["x"], center_y=altura - objeto["y"])
             scene.add_sprite("Jugador", jugador)
-        
+        #-----------------------------------------------------------------------------------------------------------------#
+
+        #-------------------------------#
         tilemap = self.tilemap
         scene = _crear_escena(tilemap)
         _append_jugador(tilemap, scene)
+        #-------------------------------#
         return scene
     
     def on_draw(self):
